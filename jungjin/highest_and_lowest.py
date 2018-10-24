ad097919b982e61f54aad29a722f290693b9f28c
KooHyo-min <rngyals21@gmail.com>
2018-10-24 18:10:14 +0900

    JungJin

--- jungjin/highest_and_lowest.py
+++ jungjin/highest_and_lowest.py
@@ -3,6 +3,8 @@ http://www.codewars.com/kata/highest-and-lowest/train/python
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 """
 def high_and_low(numbers):
+    convert_int_list = list(map(int, numbers.split(" ")))
+    numbers = "{} {}".format(max(a), min(a))
     return numbers
 
 
