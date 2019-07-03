17fd33951e7c058d21131d02d4170f7437c3c9a4
jz0912 <jz0912@naver.com>
2019-07-03 14:20:14 +0900

    ordered count of characters question

--- junyeong/ordered_count_of_characters.py
+++ junyeong/ordered_count_of_characters.py
@@ -1,7 +1,13 @@
 from junyeong import Test
 
 def ordered_count(input):
-    pass
+    result = {}
+    for c in input:
+        if c in result:
+            result[c] += 1
+        else:
+            result[c] = 1
+    return list(result.items())
 
 def test_ordered_count():
     test = Test()
@@ -9,7 +15,10 @@ def test_ordered_count():
 
     tests = (
         ('abracadabra', [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)]),
-        ('Code Wars', [('C', 1), ('o', 1), ('d', 1), ('e', 1), (' ', 1), ('W', 1), ('a', 1), ('r', 1), ('s', 1)])
+        ('Code Wars', [('C', 1), ('o', 1), ('d', 1), ('e', 1), (' ', 1), ('W', 1), ('a', 1), ('r', 1), ('s', 1)]),
+        ('Junyeong', [('J', 1), ('u', 1), ('n', 2), ('y', 1), ('e', 1), ('o', 1), ('g', 1)]),
+        ('a1b2c3a4', [('a', 2), ('1', 1), ('b', 1), ('2', 1), ('c', 1), ('3', 1), ('4', 1)]),
+        ('한글한글', [('한', 2), ('글', 2)]),
     )
 
     for t in tests:
